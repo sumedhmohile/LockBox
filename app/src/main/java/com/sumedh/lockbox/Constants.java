d96cd202e368ded9399e8a612a0ad30fb475dfbe
sumedh <summm02@gmail.com>
2020-07-06 23:16:52 +0530

    Cleaned code

--- app/src/main/java/com/sumedh/lockbox/Constants.java
+++ app/src/main/java/com/sumedh/lockbox/Constants.java
@@ -1,11 +1,13 @@
 package com.sumedh.lockbox;
 
 public class Constants {
-    public static final long SPLASH_SCREEN_DURATION = 3000L;
+    public static final long SPLASH_SCREEN_DURATION = 1L;
     public static final int TEXT_FIELD_MIN_LENGTH = 1;
     public static final String PREFERENCES = "PREFERENCES";
     public static final String USERNAME = "username";
     public static final String EMAIL = "email";
     public static final String USER_ID = "userId";
     public static final String USERS = "users";
+    public static final String BOXES = "boxes";
+    public static final String BOX_ID = "boxId";
 }
