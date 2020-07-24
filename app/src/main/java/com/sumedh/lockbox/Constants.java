906a5ee81df770aa099befc6cc59adc1676612ce
sumedh <summm02@gmail.com>
2020-07-24 23:19:58 +0530

    Updated BoxManager to add storage urls during Box creation

--- app/src/main/java/com/sumedh/lockbox/Constants.java
+++ app/src/main/java/com/sumedh/lockbox/Constants.java
@@ -24,5 +24,5 @@ public class Constants {
     public static final String BOX_CHECKIN_DATE = "lastCheckinDate";
     public static final String BOX_CHECKING_FREQUENCY = "checkingFrequency";
     public static final String NONE = "NONE";
-
+    public static final String FILES_LIST = "files";
 }
